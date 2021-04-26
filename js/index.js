cb82ca7d4b6f8a06e90686895eac8a1f9b70be25
emcrusta <emcrusta@gmail.com>
2021-04-26 11:41:18 -0600

    add all project files

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const topButton = document.getElementById('topBtn');
+
+const scrollFunction = () => {
+	if (
+		document.body.scrollTop > 500 ||
+		document.documentElement.scrollTop > 500
+	) {
+		topButton.style.visibility = 'visible';
+		topButton.style.opacity = '1';
+		topButton.style.transitionDelay = '0s';
+	} else {
+		topButton.style.visibility = 'hidden';
+		topButton.style.opacity = '0';
+		topButton.style.webkitTransition =
+			'visibility 0s linear 0.5s, opacity 0.5s linear';
+	}
+};
+
+const topFunction = () => {
+	document.body.scrollTop = 0;
+	document.documentElement.scrollTop = 0;
+};
+
+window.onscroll = () => {
+	scrollFunction();
+};
